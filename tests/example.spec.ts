22833954fa268ce6b3ca64bb37cfa1bcf1e5a7da
joaquin <joaquin@mail.com>
2025-02-23 20:47:01 -0400

    add Playwright testing framework and configuration

---
+++ tests/example.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('leyendo peliculas', async ({ page }) => {
+  await page.goto('https://cinecenter.com.bo/');
+  // espero a que cargue 20 segundos la pagina
+  await page.waitForTimeout(20000);
+  // guardo una captura 
+  await page.screenshot({ path: `cinecenter.png` });
+  // obetenemos el modal que tiene la clase modal-content
+  const boton = await page.$('.btn-cerrar');
+  // si el modal existe lo cerramos
+  if (boton) {
+    // intento hacer clic en el botón de cierre dentro del modal
+    await boton.click();
+    await page.waitForTimeout(2000);
+
+  }
+  // cargamos capturamos la pantalla
+  await page.screenshot({ path: `cinecenter2.png` });
+});
